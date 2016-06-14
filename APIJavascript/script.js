e25961e7fcb328d88565de7043a459c1ca23b1e0
Stacy <swangny@gmail.com>
2016-06-14 01:07:03 -0400

    added JukeBox and APIJavascript assignments

--- APIJavascript/script.js
+++ APIJavascript/script.js
@@ -1,31 +1,15 @@
 $(document).ready(function(){
-  $("#search").keyup("submit",function(e){ //replace keyup with on
-    e.preventDefault();
-    var formData = {
-      'q'              : $('input[name=q]').val(),
-      'type'             : $('input[name=type]').val()
-    };
-    $.ajax({
-      type: "GET",
-      url: 'https://api.spotify.com/v1/search',
-      data: formData,
-      success: function (response) {
-        var artistArray= response.artists.items; 
-        for(var i = 0 ; i<artistArray.length; i++){
-          for(var j = 0; j<artistArray[i].images.length; j++){
-            console.log(artistArray[i].images[j].url);
-            console.log(artistArray[i].name);
-            var img = new Image();
-            img.src = artistArray[i].images[j].url;
-            img.width= artistArray[i].images[j].width;
-            img.height=artistArray[i].images[j].height;
-            div1.appendChild(img);
-            var div = document.getElementById('div1');
-            var name = document.createTextNode(artistArray[i].name);
-            div.appendChild(name);
-          }
-        }
-      }
+  SC.initialize({
+    client_id: "76a808222b91b5fb454ef550c27bc2a5",
+  });
+
+  $("#hover").on('mouseover', function() {
+    var player = $("#player");
+    console.log("help");
+    SC.oEmbed('https://soundcloud.com/lin-ochoa/in-the-yard', {
+      maxheight: 200
+    }, function(res) {
+      $("#player").html(res.html);
     });
   });
 });
